771388f4a01d5f8be6607393aa00d664ced6145c
martinraul <martinracca@gmail.com>
2020-08-13 09:24:44 -0300

    Updates

---
+++ precache-manifest.2af4f592c69c6f6129804387d0cb62a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76147d39f0ed3d0dc45d93551550ace2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72cfb7e3121acc586688",
+    "url": "/static/css/main.01e22b3d.chunk.css"
+  },
+  {
+    "revision": "5e9069530416c0ff96e0",
+    "url": "/static/js/2.28ffda63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28ffda63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72cfb7e3121acc586688",
+    "url": "/static/js/main.7a9d1a9e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "be4e6933b6a1b22996e1aba21c4bd703",
+    "url": "/static/media/logo.be4e6933.jpg"
+  }
+]);
