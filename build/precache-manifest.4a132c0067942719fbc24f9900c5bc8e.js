b04e77eba724406677831903b5d9e3f2ffe07c22
martinraul <martinracca@gmail.com>
2020-11-10 08:22:13 -0300

    fixed gitIgnore

---
+++ build/precache-manifest.4a132c0067942719fbc24f9900c5bc8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2fdc8395450bfeb308d30f03f180965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cea73088bd0963adaeca",
+    "url": "/static/css/main.1515c638.chunk.css"
+  },
+  {
+    "revision": "bac05c00ccf7d107fef8",
+    "url": "/static/js/2.cd3710f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd3710f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea73088bd0963adaeca",
+    "url": "/static/js/main.742c098a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "be4e6933b6a1b22996e1aba21c4bd703",
+    "url": "/static/media/logo.be4e6933.jpg"
+  }
+]);
