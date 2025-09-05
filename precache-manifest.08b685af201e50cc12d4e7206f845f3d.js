d937a872b2da784cf440589a73e46f3342740a61
martinraul <martinracca@gmail.com>
2025-09-05 14:12:04 -0300

    Updates

---
+++ precache-manifest.08b685af201e50cc12d4e7206f845f3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c826f72a6f3755b11d55eddc2f470910",
+    "url": "/Jose-Larralde-Quotes-Generator/index.html"
+  },
+  {
+    "revision": "c226ccb86fc1599cc930",
+    "url": "/Jose-Larralde-Quotes-Generator/static/css/main.831ccdea.chunk.css"
+  },
+  {
+    "revision": "c23240967cd18fd7671c",
+    "url": "/Jose-Larralde-Quotes-Generator/static/js/2.b774065f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Jose-Larralde-Quotes-Generator/static/js/2.b774065f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c226ccb86fc1599cc930",
+    "url": "/Jose-Larralde-Quotes-Generator/static/js/main.726ae41e.chunk.js"
+  },
+  {
+    "revision": "289fc40436ebfb1f850b",
+    "url": "/Jose-Larralde-Quotes-Generator/static/js/runtime-main.80d0217f.js"
+  },
+  {
+    "revision": "b12106d13ae531d3a37804a4c6d9a29f",
+    "url": "/Jose-Larralde-Quotes-Generator/static/media/background1.b12106d1.jpg"
+  },
+  {
+    "revision": "be4e6933b6a1b22996e1aba21c4bd703",
+    "url": "/Jose-Larralde-Quotes-Generator/static/media/logo.be4e6933.jpg"
+  }
+]);
